18f8c5117d5300443c699cf9115526eece988091
Alex_Coder <dima.boy.alex@gmail.com>
2017-12-02 02:33:58 +0200

    Second commit

--- bad_test/bad_test.py
+++ bad_test/bad_test.py
@@ -1,5 +1,5 @@
 import time
- 
+import statistics
 class Profiler(object):
     def __enter__(self):
         self._startTime = time.time()
@@ -8,24 +8,12 @@ class Profiler(object):
         print("Elapsed time: {:.3f} sec ".format(time.time() - self._startTime))
 
 with Profiler() as p:
-    file = open('10m.txt')
-    text = file.read()
-    array = text.split('\n')
+    array = open('10m.txt').read().split('\n')
     array.pop()
-    #print(array)
-    result = []
-    for i in array:
-        if i[0] == '-':
-            result.append(0 - float(i[1:]))
-        elif i:
-            result.append(float(i))
-    #print(result)        
-    array_max = max(result)
-    array_min = min(result)
-    print("min = %s ,\
-          max = %s" % (array_min, array_max))
-
-
+    floats = [float(x) for x in array]
+    print("max = %s\nmin = %s\nmediana = %s\naverage = %s"
+          % (max(floats), min(floats), statistics.median(floats), statistics.mean(floats)) )
+   
 
 
 
